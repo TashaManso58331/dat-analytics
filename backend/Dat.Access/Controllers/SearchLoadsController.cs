b0bd5e4584d17f541517d53f4231b9e4b27a62d0
Yuri Vasilyev <yuravas.dm@outlook.com>
2021-10-16 22:28:56 +0300

    append SearchLoads controller

---
+++ backend/Dat.Access/Controllers/SearchLoadsController.cs
@@ -0,0 +1,42 @@
+﻿using Dat.Access.Caches;
+using Dat.Access.Clients;
+using Dat.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Dat.Access.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchLoadsController : ControllerBase
+    {
+        private readonly ILogger<SearchLoadsController> log;
+        private readonly SimpleMemoryCache<AccessToken> userTokenCache;
+        private readonly string userAccount;
+
+        public SearchLoadsController(ILogger<SearchLoadsController> logger, IMemoryCache memoryCache, IConfiguration config, IDatService datClient)
+        {
+            this.log = logger;
+            this.userTokenCache = new SimpleMemoryCache<AccessToken>(memoryCache);
+            this.userAccount = config[AccessController.cUserAccount] ?? throw new ArgumentNullException(AccessController.cUserAccount);
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            try
+            {
+                var userToken = userTokenCache.GetOrCreate(userAccount, () => throw new ArgumentException("User token is not in cache"));
+                return userToken?.Token;
+            }
+            catch (Exception ex)
+            {
+                log.LogError(ex, "Failed to get user token sessionAccount={0} userAccount={1}");
+                throw;
+            }
+        }
+    }
+}
